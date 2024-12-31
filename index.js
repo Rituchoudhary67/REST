658717c0073cd8d247ab26f10b940d560f4ad6ae
Ritu Choudhary <ritulalitchoudhay@gmail.com>
2024-12-31 13:48:45 +0530

    added edit functionalit

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+//for creating unique id
+const { v4: uuidv4 } = require('uuid');
+//for overrideing the html form
+const methodOverride = require("method-override");
+
 
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 
@@ -13,17 +19,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [    
     {
-        id: "1a",
+        id: uuidv4(),
         username : "apnacollege",
         content : "I love coding",
     },
     {
-        id:"2b",
+        id:uuidv4(),
         username : "shradha Didi",
         content : "Hard work is important to acchive success",
     },
     {
-        id:"3c",
+        id:uuidv4(),
         username : "Ritu choudhary",
         content : "I got selected for my 1st internship!",
     },
@@ -41,7 +47,8 @@ app.get("/posts", (req,res) => {
 
 app.post("/posts", (req,res) => {
     let {username,content} = req.body;
-    posts.push({username,content});
+    let id = uuidv4();
+    posts.push({id,username,content});
     res.redirect("/posts");
 })
 
@@ -51,6 +58,21 @@ app.get("/posts/:id", (req,res) => {
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs",{post});
 });
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req,res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs",{ post });
+})
 
 app.listen(port, () => {
     console.log("listening to port : 8080");
